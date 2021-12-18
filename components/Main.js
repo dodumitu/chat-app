7ee88c89375af42c4d0e39e69471fc1e1fa28cae
Đỗ Duy Minh Tuấn <80260231+dodumitu@users.noreply.github.com>
2021-12-18 19:44:10 +0700

    update

--- components/Main.js
+++ components/Main.js
@@ -1,33 +1,44 @@
-import Content from "./content.js";
 import ConversationList from "./ConversationList.js";
-class Main {
-  $mainContainer;
-  $conversationList;
-  $content;
-  constructor() {
-    this.$mainContainer = document.createElement("div");
-    this.$mainContainer.setAttribute(
-      "class",
-      "flex w-screen h-screen bg-gray-400"
-    );
-    this.$conversationList = new ConversationList(this.changeActive);
+import Title from "./Title.js";
+import MessageList from "./MessageList.js";
+import MessageInput from "./MessageInput.js";
 
-    this.$content = new Content();
-    this.$content.setValue("Chat-App");
+export default class Main {
+  $conversationList;
+  $title;
+  $messageList;
+  $messageInput;
+
+  constructor() {
+    this.$title = new Title("No data", 0);
+    this.$messageList = new MessageList();
+    this.$messageInput = new MessageInput();
+
+    this.$conversationList = new ConversationList((conversation) => {
+      this.setActiveConversation(conversation);
+    });
   }
 
-  changeActive = (data) => {
-    this.$content.setValue("");
-    this.$content.setValue(data);
+  setActiveConversation = (conversation) => {
+    this.$title.setActiveConversation(conversation);
+    this.$messageList.setActiveConversation(conversation);
+    this.$messageInput.setActiveConversation(conversation);
   };
 
   render(container) {
-    this.$conversationList.render(this.$mainContainer);
+    const mainContainer = document.createElement("div");
+    const content = document.createElement("div");
 
-    this.$content.render(this.$mainContainer);
+    mainContainer.setAttribute("class", "flex w-screen h-screen bg-gray-400");
+    content.setAttribute("class", "flex flex-col w-full h-full px-24 bg-white");
 
-    container.appendChild(this.$mainContainer);
+    this.$title.render(content);
+    this.$messageList.render(content);
+    this.$messageInput.render(content);
+
+    this.$conversationList.render(mainContainer);
+    mainContainer.appendChild(content);
+
+    container.appendChild(mainContainer);
   }
 }
-
-export default Main;
